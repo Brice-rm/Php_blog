5574d149236bbea835b2ea96ad6388840547b17e
Brice <72446977+Brice-rm@users.noreply.github.com>
2021-02-08 11:55:20 +0100

    Lundi

---
+++ View/inc/footer.php
@@ -0,0 +1,17 @@
+
+
+
+<footer class="page-footer blue-grey lighten-1 center">
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="footer-column ">
+        <div class="col l6 s12">
+          Copyright © <?= date("Y"); ?> Brice ROMÉ
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <a href="<?=ROOT_URL?>blog_legalNotice.html" class="white-text">Mentions légales</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ bdd.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 08 fév. 2021 à 10:48
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `blog-ecrivain`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `comment` text CHARACTER SET utf8 NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `seen` tinyint(11) NOT NULL DEFAULT '0',
+  `signals` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=97 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`id`, `user_id`, `comment`, `post_id`, `date`, `seen`, `signals`) VALUES
+(93, 14, 'Pas mal !', 40, '2018-02-22 07:20:01', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `body` longtext NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'post.png',
+  `createdDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=49 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `image`, `createdDate`) VALUES
+(45, 'chateau', '<p>:yfi:u&lt;pym!humoih!PK%p&ugrave;kihgjuyftfgyjhujiokpl^mplkoj!ih:guyftg</p>', '47.png', '2021-02-08 11:05:05'),
+(40, 'Pub Bar', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida. Nisl rhoncus mattis rhoncus urna neque viverra. Faucibus interdum posuere lorem ipsum dolor sit amet. Mattis ullamcorper velit sed ullamcorper morbi. Senectus et netus et malesuada fames. Vulputate odio ut enim blandit volutpat maecenas volutpat. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. Non arcu risus quis varius. Diam vulputate ut pharetra sit amet aliquam id diam. Nibh tortor id aliquet lectus proin nibh nisl condimentum. Sit amet cursus sit amet dictum sit amet.</p>\r\n<p><img class=\"\" src=\"static/source/petite%20image.jpg\" alt=\"\" width=\"245\" height=\"232\" /></p>\r\n<p>Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Fermentum iaculis eu non diam. Ultrices sagittis orci a scelerisque purus semper. Ac turpis egestas sed tempus urna. Dui accumsan sit amet nulla facilisi. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Varius sit amet mattis vulputate enim nulla aliquet porttitor. Ullamcorper dignissim cras tincidunt lobortis feugiat. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Condimentum vitae sapien pellentesque habitant morbi tristique. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Leo a diam sollicitudin tempor id eu nisl nunc mi. Neque volutpat ac tincidunt vitae semper quis lectus nulla.</p>\r\n<p>Quis blandit turpis cursus in hac habitasse platea dictumst quisque. Tincidunt vitae semper quis lectus nulla at volutpat. Sed sed risus pretium quam vulputate dignissim suspendisse in est. Duis at tellus at urna condimentum mattis. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Sed odio morbi quis commodo. In vitae turpis massa sed elementum tempus egestas. Dui ut ornare lectus sit amet. In pellentesque massa placerat duis ultricies. Accumsan lacus vel facilisis volutpat est velit egestas dui. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Cursus vitae congue mauris rhoncus aenean vel. Lacus luctus accumsan tortor posuere.</p>', '31.jpg', '2018-02-07 13:41:04'),
+(48, 'Château', '<p>:yfi:u&lt;pym!humoih!PK%p&ugrave;kihgjuyftfgyjhujiokpl^mplkoj!ih:guyftg</p>', '_visite-bateau-strasbourg-credit-AAA-JCH@700x465.jpg', '2021-02-08 11:30:27'),
+(46, 'La plage', '<p>dwhgvsPGqrhggih\"itzitph\"&ugrave;ptoTOH\'zia&ugrave;tptOJTYjo(\'t</p>\r\n<p>ji&ugrave;uoazy\"</p>', '46.jpg', '2021-02-08 11:25:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(120) NOT NULL,
+  `password` char(60) NOT NULL,
+  `admin` int(11) DEFAULT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `admin`, `pseudo`) VALUES
+(8, 'test@test.com', '72cc3a193b146557606c9fe4b605d43ac43d12a8', 0, 'Romain'),
+(14, 'tom@gmail.com', '21e57080434c1cb6c2fb4753c4f8a28f', NULL, 'Tom'),
+(17, 'bricerome77@hotmail.fr', '7c4a8d09ca3762af61e59520943dc26494f8941b', 3, 'Brice');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `votes`
+--
+
+DROP TABLE IF EXISTS `votes`;
+CREATE TABLE IF NOT EXISTS `votes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment_id` int(11) NOT NULL,
+  `user_id` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `vote` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=136 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
